a1448da727591e310984dce98820934ed33ed81a
Elias Granja <contato@eliasgranja.com>
2014-03-19 15:17:08 -0300

    Leting container be a public variable

--- geoffrey.js
+++ geoffrey.js
@@ -7,20 +7,22 @@ app.get('/:pg', function(req, res){
 	res.render(req.params.pg);
 });
 
-Geofrey.serves = function(views_folder) {
+Geoffrey.serves = function(views_folder) {
 	app.set('views', views_folder);
 	return this;
 }
 
-Geofrey.with = function(public_folder) {
+Geoffrey.with = function(public_folder) {
 	app.use(express.static( public_folder));
 	return this;
 }
 
-Geofrey.on = function(port) {
+Geoffrey.on = function(port) {
 	app.listen(port);
 	console.log('Server running at http://127.0.0.1:'+port);
 	return this;
 }
+
+Geoffrey.container = app;
 
 module.exports = Geofrey;
